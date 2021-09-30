f3eea1c22ded64fccafdda9be22d035bfe5e4216
Richard Mora <r.mora@students.clark.edu>
2021-09-29 19:26:31 -0700

    Project uploaded

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Project-1-DTC-355
+
+## For this website, I combined cards with some background images while using CSS Grid
+
+## This project was for a university course
---
+++ js/style.js
@@ -0,0 +1,248 @@
+let stanza1 = document.getElementById("stanza1");
+
+let stanza2 = document.getElementById("stanza2");
+
+let stanza3 = document.getElementById("stanza3");
+
+let stanza4 = document.getElementById("stanza4");
+
+let stanza5 = document.getElementById("stanza5");
+
+let stanza6 = document.getElementById("stanza6");
+
+let stanza7 = document.getElementById("stanza7");
+
+let stanza8 = document.getElementById("stanza8");
+
+let stanza9 = document.getElementById("stanza9");
+
+let stanza10 = document.getElementById("stanza10");
+
+let stanza11 = document.getElementById("stanza11");
+
+let stanza12 = document.getElementById("stanza12");
+
+let stanza13 = document.getElementById("stanza13");
+
+function stanza1Change() {
+  stanza1.innerHTML =
+    "<h3>I</h3>" +
+    "Among twenty snowy mountains,<br>" +
+    "The only moving thing <br>" +
+    "Was the eye of the blackbird.";
+}
+
+function back1() {
+  stanza1.innerHTML = "<h3>I</h3>";
+}
+
+stanza1.onmouseover = function () {
+  stanza1Change();
+}
+
+stanza1.onmouseout = function () {
+  back1();
+}
+
+function stanza2Change() {
+  stanza2.innerHTML =
+    "<h3>II</h3>" +
+    "I was of three minds, <br>" +
+    "Like a tree <br>" +
+    "In which there are three blackbirds.";
+}
+
+function back2() {
+  stanza2.innerHTML = "<h3>II</h3>";
+}
+
+stanza2.onmouseover = function () {
+  stanza2Change();
+}
+
+stanza2.onmouseout = function () {
+  back2();
+}
+
+function stanza3Change() {
+  stanza3.innerHTML =
+    "<h3>III</h3>" +
+    "The blackbird whirled in the autumn winds.<br>" +
+    "It was a small part of the pantomine.";
+}
+
+function back3() {
+  stanza3.innerHTML = "<h3>III</h3>";
+}
+
+stanza3.onmouseover = function () {
+  stanza3Change();
+}
+
+stanza3.onmouseout = function () {
+  back3();
+}
+
+function stanza4Change() {
+  stanza4.innerHTML =
+    "<h3>IV</h3>" +
+    "A man and a woman <br>" +
+    "Are one.<br>" +
+    "A man and a woman and a blackbirds.<br>" +
+    "Are one.";
+}
+
+function back4() {
+  stanza4.innerHTML = "<h3>IV</h3>";
+}
+
+stanza4.onmouseover = function () {
+  stanza4Change();
+}
+
+stanza4.onmouseout = function () {
+  back4();
+}
+
+function stanza5Change() {
+  stanza5.innerHTML =
+    "<h3>V</h3>" +
+    "I do not know which to prefer, <br>" +
+    "The beauty of infections <br>" +
+    "Or the beauty of innuendoes, <br>" +
+    "The blackbird whistling <br>" +
+    "Or just after.";
+}
+
+function back5() {
+  stanza5.innerHTML = "<h3>V</h3>";
+}
+
+stanza5.onmouseover = function () {
+  stanza5Change();
+}
+
+stanza5.onmouseout = function () {
+  back5();
+}
+
+function stanza6Change() {
+  stanza6.innerHTML =
+    "<h3>VI</h3>" +
+    "Icicles filled the long window <br>" +
+    "With barbaric glass. <br>" +
+    "The shadow of the blackbird <br>" +
+    "Crossed it, to and fro. <br>" +
+    "The mood <br>" +
+    "Traced in the shadow <br>" +
+    "An indecipherable cause.";
+}
+
+function back6() {
+  stanza6.innerHTML = "<h3>VI</h3>";
+}
+
+stanza6.onmouseover = function () {
+  stanza6Change();
+}
+
+stanza6.onmouseout = function () {
+  back6();
+}
+
+function stanza7Change () {
+    stanza7.innerHTML = '<h3>VII</h3>'+'O thin men of Haddam, <br>' +
+    'Why do you imagine golden birds?<br>' +
+    'Walks around the feet <br>' + 'Of the women about you?';
+}
+
+function back7 () {
+    stanza7.innerHTML = '<h3>VII</h3>';
+}
+
+stanza7.onmouseover = function() {stanza7Change ()}
+
+stanza7.onmouseout = function() {back7()}
+
+function stanza8Change () {
+    stanza8.innerHTML = '<h3>VIII</h3>'+'I know noble accents <br>' +
+    'And lucid, inescapable rhythms; <br>' +
+    'But I know, too, <br>' +'That the blackbird is involved <br>' + 'In what I know.';
+}
+
+function back8 () {
+    stanza8.innerHTML = '<h3>VIII</h3>';
+}
+
+stanza8.onmouseover = function() {stanza8Change ()}
+
+stanza8.onmouseout = function() {back8()}
+
+function stanza9Change () {
+    stanza9.innerHTML = '<h3>IX</h3>'+'When the blackbird flew out of sight, <br>' +
+    'It marked the edge <br>' +
+    'Of one of many circles.';
+}
+
+function back9 () {
+    stanza9.innerHTML = '<h3>IX</h3>';
+}
+
+stanza9.onmouseover = function() {stanza9Change ()};
+
+stanza9.onmouseout = function() {back9()};
+
+function stanza10Change () {
+    stanza10.innerHTML = '<h3>X</h3>'+'At the sight of blackbirds <br>' +
+    'Flying in a green light, <br>' +
+    'Even the bawds of euphony <br>' +'Would cry out sharply.';
+}
+
+function back10 () {
+    stanza10.innerHTML = '<h3>X</h3>';
+}
+
+stanza10.onmouseover = function() {stanza10Change ()};
+
+stanza10.onmouseout = function() {back10()};
+
+function stanza11Change () {
+    stanza11.innerHTML = '<h3>XI</h3>'+'He rode over Connecticut <br>' +
+    'In a glass coach.<br>' +
+    'Once, a fear pierced him, <br>' + 'In that he mistook <br>' + 'The shadow of his equipage <br>' + 'For blackbirds.';
+}
+
+function back11 () {
+    stanza11.innerHTML = '<h3>XI</h3>';
+}
+
+stanza11.onmouseover = function() {stanza11Change ()};
+
+stanza11.onmouseout = function() {back11()};
+
+function stanza12Change () {
+    stanza12.innerHTML = '<h3>XII</h3>'+'The river is moving. <br>' +
+    'The blackbird must by flying.';
+}
+
+function back12 () {
+    stanza12.innerHTML = '<h3>XII</h3>';
+}
+
+stanza12.onmouseover = function() {stanza12Change ()};
+
+stanza12.onmouseout = function() {back12()};
+
+function stanza13Change () {
+    stanza13.innerHTML = '<h3>XIII</h3>'+'It was evening all afternoon.<br>' +
+    'It was snowing <br>' +
+    'And it was going to snow. <br>' + 'The blackbird sat <br>' + 'In the cedar-limbs.';
+}
+
+function back13 () {
+    stanza13.innerHTML = '<h3>XIII</h3>';
+}
+
+stanza13.onmouseover = function() {stanza13Change ()};
+
+stanza13.onmouseout = function() {back13()};
